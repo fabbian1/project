fc4e179b0df25884cd9f614f2f727ce641bc4b3c
fabbian1 <nfhambira@hotmail.com>
2024-08-13 23:34:33 +0800

    verify that push works fine

--- scriptVerifyPush.sh
+++ scriptVerifyPush.sh
@@ -2,3 +2,4 @@
 echo "Welcome to git tutorial"
 echo "Sit back relax and enjoy the process"
 echo "this script tests if indeed push is working correctly or not"
+echo "It is working just fine, no issues!"
